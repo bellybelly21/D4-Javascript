1e4c3982f6a359c65096e4f76d43a3da83bad4e9
Sybella Sandoval <sybellasss@gmail.com>
2024-07-11 02:03:31 -0400

    Ahora si debería funcionar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-const propiedades_venta = [
+const propiedadesVenta = [
     {
 nombre: 'Casa en venta Vitacura',
-src: '../img/propiedad-venta1.jpg',
+src: 'assets/img/propiedad-venta1.jpg',
 descripcion: 'Espectacular casa en hormigón armado del afamado arquitecto Christian de Groote con espectacular vista a la cordillera.',
 ubicacion: 'Vitacura, RM',
 superficie: '1.300 m2',
@@ -13,7 +13,7 @@ pets: true
 
 {
 nombre: 'Casa de alto standing Viña del Mar',
-src: '../img/propiedad-venta2.jpg',
+src: 'assets/img/propiedad-venta2.jpg',
 descripcion: 'Espectacular casa de estilo mediterráneo de 1.187 metros cuadrados emplazada en un terreno único de 2.720 metros cuadrados.',
 ubicacion: 'Viña del Mar, RV',
 superficie: '1.187 m2',
@@ -37,7 +37,7 @@ pets: true
 
 {
 nombre: 'Exclusiva Villa/Chalet en Lo Barnechea',
-src: '/assets/img/propiedad-venta4.jpg',
+src: 'assets/img/propiedad-venta4.jpg',
 descripcion: 'Casa italiana del arquitecto Jaime Burgos, ubicada en sector Los Nogales, con finas terminaciones y grandes espacios.',
 ubicacion: 'Lo Barnechea, RM',
 superficie: '675 m2',
@@ -49,7 +49,7 @@ pets: false
 
 {
 nombre: 'Casa de alto standing en venta Las Condes',
-src: '../img/propiedad-venta5.jpg',
+src: 'assets/img/propiedad-venta5.jpg',
 descripcion: 'Casa de estilo Inglés, con materiales nobles como madera pino Oregón, ladrillos y piedra laja. Con gran jardín formado y grandes árboles.',
 ubicacion: 'Las Condes, RM',
 superficie: '590 m2',
@@ -61,7 +61,7 @@ pets: true
 
 {
 nombre: 'Mansión en venta Vitacura, Chile',
-src: '../img/propiedad-venta6.jpg',
+src: 'assets/img/propiedad-venta6.jpg',
 descripcion: 'Casa del destacado arquitecto Jorge Figueroa, con privilegiada vista a las canchas y a la ciudad, emplazada a los pies del cerro Manquehue.',
 ubicacion: 'Vitacura, RM',
 superficie: '1.064 m2',
@@ -72,10 +72,10 @@ pets: true
 }
 ];
 
-const propiedades_alquiler = [
+const propiedadesAlquiler = [
     {
 nombre: 'Piso exclusivo en alquiler en Vitacura',
-src: '../img/propiedad-alquiler1.jpg',
+src: 'assets/img/propiedad-alquiler1.jpg',
 descripcion: 'Edificio de finas terminacion Departamento duplex, penthouse de un edificio de 11 pisos. Es un departamento que está diseñado por profesionales.',
 ubicacion: 'Vitacura, RM',
 superficie: '555 m2',
@@ -87,7 +87,7 @@ pets: true
 
 {
 nombre: 'Casa en alquiler en Las Condes',
-src: '../img/propiedad-alquiler2.jpg',
+src: 'assets/img/propiedad-alquiler2.jpg',
 descripcion: 'Diseñada por el arquitecto Carlos Alberto Cruz, esta propiedad de Estilo Europeo en Lo Curro disfruta de una imponente vista panorámica de la ciudad y la cordillera.',
 ubicacion: 'Las Condes, RM',
 superficie: '1.605 m2',
@@ -99,7 +99,7 @@ pets: false
 
 {
 nombre: 'Exclusiva casa en alquiler en Vitacura',
-src: '../img/propiedad-alquiler3.jpg',
+src: 'assets/img/propiedad-alquiler3.jpg',
 descripcion: 'Casa en gran terreno, parque con vista y senderos de piedra, de construcción solida de 2 pisos, además de un subterráneo con luz natural.',
 ubicacion: 'Vitacura, RM',
 superficie: '750 m2',
@@ -111,7 +111,7 @@ pets: true
 
 {
 nombre: 'Casa en Barrio Jardín del Este Vitacura',
-src: '../img/propiedad-alquiler4.jpg',
+src: 'assets/img/propiedad-alquiler4.jpg',
 descripcion: 'Icónica casa ubicada en pleno barrio Jardín del Este en Vitacura, de arquitectura moderna de los años ’70, obra del reconocido arquitecto chileno-francés Mauricio Despuy.',
 ubicacion: 'Vitacura, RM',
 superficie: '405 m2',
@@ -123,7 +123,7 @@ pets: true
 
 {
 nombre: 'Casa de alto standing en alquiler Las Condes',
-src: '../img/propiedad-alquiler5.jpg',
+src: 'assets/img/propiedad-alquiler5.jpg',
 descripcion: 'Increíble casa tradicional, con Hall de entrada con acceso a amplio living y comedor separados, salida a terraza y a gran jardín con piscina.',
 ubicacion: 'Las Condes, RM',
 superficie: '435 m2',
@@ -135,7 +135,7 @@ pets: false
 
 {
 nombre: 'Exclusiva casa en alquiler Las Condes',
-src: '../img/propiedad-alquiler6.jpg',
+src: 'assets/img/propiedad-alquiler6.jpg',
 descripcion: 'Linda casa estilo Inglesa ubicada en un pasaje cerrado de 3 casas, excelente conectividad e inigualable ubicación. Destaca por sus amplios espacios.',
 ubicacion: 'Las Condes, RM',
 superficie: '420 m2',
@@ -146,12 +146,16 @@ pets: false
 }
 ];
 
+document.addEventListener('DOMContentLoaded', () => {
+    fichaPropiedades(propiedadesVenta, 6, ".ficha-venta");
+    fichaPropiedades(propiedadesAlquiler, 6, ".ficha-alquiler");
+});
 
-function fichaPropiedades_venta(numPropiedades) {
-    const divFicha = document.querySelector(".ficha-venta");
+function fichaPropiedades(propiedades, numPropiedades, contenedorClase) {
+    const divFicha = document.querySelector(contenedorClase);
     
     let template = '';
-    for (const propiedad of propiedades_venta.slice(0, numPropiedades)) {
+    for (const propiedad of propiedades.slice(0, numPropiedades)) {
         let smokeColor = propiedad.smoke ? 'green' : 'red';
         let petsColor = propiedad.pets ? 'green' : 'red';
 
@@ -159,18 +163,18 @@ function fichaPropiedades_venta(numPropiedades) {
         <div class="propiedad-template">
             <img src="${propiedad.src}" class="img-template">
             <div class="propiedad-contenido">
-            <p class="propiedad-ubicacion">${propiedad.ubicacion}</p>
-            <h4>${propiedad.nombre}</h4>
-            <p class="propiedad-sup-hab">
-            <span><i class="fa-solid fa-house"></i> ${propiedad.superficie}</span>
-            <span><i class="fa-solid fa-bed"></i> ${propiedad.habitaciones} Habitaciones</span>
-            </p>
-            <p class="propiedad-descripcion">${propiedad.descripcion}</p>
-            <h5>$${propiedad.costo} USD</h5>
-            <hr>
-            <span class="propiedad-pets-smoke"><span style="color: ${smokeColor}"><i class="fa-solid fa-smoking"></i> ${propiedad.smoke ? 'Permitido fumar' : 'No se permite fumar'}</span>
-            <span style="color: ${petsColor}"><i class="fa-solid fa-paw"></i> ${propiedad.pets ? 'Mascotas permitidas' : 'No se permiten mascotas'}</span>
-            </span>
+                <p class="propiedad-ubicacion">${propiedad.ubicacion}</p>
+                <h4>${propiedad.nombre}</h4>
+                <p class="propiedad-sup-hab">
+                <span><i class="fa-solid fa-house"></i> ${propiedad.superficie}</span>
+                <span><i class="fa-solid fa-bed"></i> ${propiedad.habitaciones} Habitaciones</span>
+                </p>
+                <p class="propiedad-descripcion">${propiedad.descripcion}</p>
+                <hr>
+                <span class="propiedad-pets-smoke">
+                    <span style="color: ${smokeColor}"><i class="fa-solid fa-smoking"></i> ${propiedad.smoke ? 'Permitido fumar' : 'No se permite fumar'}</span>
+                    <span style="color: ${petsColor}"><i class="fa-solid fa-paw"></i> ${propiedad.pets ? 'Mascotas permitidas' : 'No se permiten mascotas'}</span>
+                </span>
             </div>
         </div>
         `;
@@ -179,37 +183,15 @@ function fichaPropiedades_venta(numPropiedades) {
     divFicha.innerHTML = template;
 }
 
-function fichaPropiedades_alquiler(numPropiedades) {
-    const divFicha = document.querySelector(".ficha-alquiler");
-    
-    let template = '';
-    for (const propiedad of propiedades_alquiler.slice(0, numPropiedades)) {
-        let smokeColor = propiedad.smoke ? 'green' : 'red';
-        let petsColor = propiedad.pets ? 'green' : 'red';
+document.addEventListener('DOMContentLoaded', () => {
+    const path = window.location.pathname;
 
-        template += `
-        <div class="propiedad-template">
-            <img src="${propiedad.src}" class="img-template">
-            <div class="propiedad-contenido">
-            <p class="propiedad-ubicacion">${propiedad.ubicacion}</p>
-            <h4>${propiedad.nombre}</h4>
-            <p class="propiedad-sup-hab">
-            <span><i class="fa-solid fa-house"></i> ${propiedad.superficie}</span>
-            <span><i class="fa-solid fa-bed"></i> ${propiedad.habitaciones} Habitaciones</span>
-            </p>
-            <p class="propiedad-descripcion">${propiedad.descripcion}</p>
-            <h5>$${propiedad.costo} USD</h5>
-            <hr>
-            <span class="propiedad-pets-smoke"><span style="color: ${smokeColor}"><i class="fa-solid fa-smoking"></i> ${propiedad.smoke ? 'Permitido fumar' : 'No se permite fumar'}</span>
-            <span style="color: ${petsColor}"><i class="fa-solid fa-paw"></i> ${propiedad.pets ? 'Mascotas permitidas' : 'No se permiten mascotas'}</span>
-            </span>
-            </div>
-        </div>
-        `;
+    if (path.includes('index.html')) {
+        fichaPropiedades(propiedadesVenta, 3, ".ficha-venta");
+        fichaPropiedades(propiedadesAlquiler, 3, ".ficha-alquiler");
+    } else if (path.includes('propiedades_venta.html')) {
+        fichaPropiedades(propiedadesVenta, 6, ".ficha-venta");
+    } else if (path.includes('propiedades_alquiler.html')) {
+        fichaPropiedades(propiedadesAlquiler, 6, ".ficha-alquiler");
     }
-    
-    divFicha.innerHTML = template;
-}
-
-fichaPropiedades_venta(3);
-fichaPropiedades_alquiler(3);
+});
